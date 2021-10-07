ea4087284c37a85597208ba63fde9292fd01abc1
Jeferson Monteiro <jefimpjjn@gmail.com>
2021-10-06 22:33:32 -0300

    sprint 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
+import { TemaComponent } from './tema/tema.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path:'entrar',component: EntrarComponent},
+  {path:'cadastrar',component: CadastrarComponent},
+  {path:'home',component: HomeComponent},
+  {path:'tema', component:TemaComponent},
+
+  {path:'tema-edit/:id', component:TemaEditComponent},
+  {path:'tema-delete/:id', component:TemaDeleteComponent},
+
+  {path:'postagem-edit/:id', component:PostagemEditComponent},
+  {path:'postagem-delete/:id', component:PostagemDeleteComponent},
+
+  {path:'user-edit/:id', component:UserEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { AlertasComponent } from './alertas/alertas.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { OrderModule } from 'ngx-order-pipe';
+import { HomeComponent } from './home/home.component';
+import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    AlertasComponent,
+    HomeComponent,
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent,
+    PostagemDeleteComponent,
+    PostagemEditComponent,
+    UserEditComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    OrderModule
+    
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
